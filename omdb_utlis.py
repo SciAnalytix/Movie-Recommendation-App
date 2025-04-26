35445aa0ce7b188fe6c323c0b2d6b78e8490587c
Prakhar Sharma <142746708+SciAnalytix@users.noreply.github.com>
2025-04-26 18:36:24 +0530

    Update omdb_utlis.py

--- omdb_utlis.py
+++ omdb_utlis.py
@@ -1,10 +1,10 @@
+
 import requests
 
-def get_movie_details(title, api_key):
-    url = f"http://www.omdbapi.com/?t={title}&plot=full&apikey={api_key}"
-    res = requests.get(url).json()
-    if res.get("Response") == "True":
-        plot = res.get("Plot", "N/A")
-        poster = res.get("Poster", "N/A")
-        return plot, poster
-    return "N/A", "N/A"
+def get_movie_details(movie_title, api_key):
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    plot = data.get('Plot', 'N/A')
+    poster = data.get('Poster', 'N/A')
+    return plot, poster
